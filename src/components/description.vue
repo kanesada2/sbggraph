57aa04be5475bdfd47cde179f9f3c5e5e84cc909
kanesada2 <kanesada2@users.noreply.github.com>
2020-10-14 23:52:31 +0900

    一時的にサーバ情報をコメントアウト

--- src/components/description.vue
+++ src/components/description.vue
@@ -4,11 +4,6 @@
         <p>このサイトは、MinecraftのSpigot用プラグイン<a href="https://www.spigotmc.org/resources/snowballgame.35339/" target="blank">SnowballGame</a>の投球・打撃データを可視化することを目的としたサイトです。
         <br>
         上部メニュー<a href="/hit">HITS</a>から打撃データを、<a href="/ball">BALLS</a>から投球データを閲覧できます。
-        <br>
-        下記のサーバでSnowballGameをプレイするとデータが記録されます。サイトへの反映はリアルタイムで行われます。より多くのデータの収集にご協力ください。
-        <br>
-        <br>
-        サーバアドレス: maeda.cf:51311
         </p>
     </div>
 </template>
